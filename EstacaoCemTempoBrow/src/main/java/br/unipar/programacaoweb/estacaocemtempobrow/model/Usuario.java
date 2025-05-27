22a2c47e31797a08142f6df583320daf9cb0cddc
Carvalhoooo01 <136996231+Carvalhoooo01@users.noreply.github.com>
2025-05-26 21:29:27 -0300

    Add files via upload

---
+++ EstacaoCemTempoBrow/src/main/java/br/unipar/programacaoweb/estacaocemtempobrow/model/Usuario.java
@@ -0,0 +1,28 @@
+package br.unipar.programacaoweb.estacaocemtempobrow.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+@Getter
+@Setter
+public class Usuario implements Serializable, UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String username;
+
+    private String password;
+}
---
+++ EstacaoCemTempoBrow/src/main/java/br/unipar/programacaoweb/estacaocemtempobrow/model/enums/statusEstacao.java
@@ -0,0 +1,17 @@
+package br.unipar.programacaoweb.estacaocemtempobrow.model.enums;
+
+public enum statusEstacao {
+    ATIVA,
+    INATIVA,
+    MANUTENCAO;
+
+    public static statusEstacao fromString(String text) {
+        for (statusEstacao status : statusEstacao.values()) {
+            if (status.name().equalsIgnoreCase(text)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Status inválido: " + text);
+    }
+}
+
